6d44441a142fe030e4b9e2e26bcb528240b2e856
SanadHantash <sanadhantash24@gmail.com>
2023-11-11 19:11:29 +0300

    done

--- server/config.js
+++ server/config.js
@@ -9,28 +9,6 @@ const pool = new Pool({
     database: process.env.DB_NAME
 });
 
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCXGyymYGCYewwK2pRozT11KYxVnfjLbHM",
-//   authDomain: "wiseassist-b8a8a.firebaseapp.com",
-//   projectId: "wiseassist-b8a8a",
-//   storageBucket: "wiseassist-b8a8a.appspot.com",
-//   messagingSenderId: "221654214715",
-//   appId: "1:221654214715:web:66d41f14bee8785798541e",
-//   measurementId: "G-61YEY3ELES"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-
 module.exports = {
   query: (text, params) => pool.query(text, params),
 };
